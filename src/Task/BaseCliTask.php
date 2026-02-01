05f99b3f4af4c4667d221e6e73b830184fe49509
Andor <david-andor@kozpontiagy.hu>
2026-02-01 17:18:01 +0100

    2.x Composer upgrade

--- src/Task/BaseCliTask.php
+++ src/Task/BaseCliTask.php
@@ -369,8 +369,9 @@ abstract class BaseCliTask extends BaseTask implements CommandInterface, OutputA
     protected function runDoIt()
     {
         // @todo Remove this once drupal/core uses symfony/process:^4.
-        $processInner =  is_callable([Process::class, 'fromShellCommandline']) ?
-            Process::fromShellCommandline($this->command)
+        // @phpstan-ignore-next-line
+        $processInner = is_callable([Process::class, 'fromShellCommandline'])
+            ? Process::fromShellCommandline($this->command)
             : new Process(['bash', '-c', $this->command]);
 
         $processInner->setTimeout($this->options['processTimeout']['value']);
